22d5dae96c95e65a40218679129db03a9257cf3d
unknown <k4sr4_r@yahoo.com>
2017-11-05 17:40:47 -0800

    Fixed the tilemap

---
+++ Assets/Scripts/TilemapScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapScript : MonoBehaviour {
+
+    public BoundsInt area;
+
+    void Start()
+    {
+        for (int y = -1; y < Grid.h + 1; ++y)
+        {
+            for (int x = 0; x < Grid.w; ++x)
+            {
+                if (GetComponent<Tilemap>().GetTile(new Vector3Int(x, y, 0)) != null)
+                {
+                    GameObject tile = Instantiate(new GameObject(), new Vector3(x, y, 0), Quaternion.identity);
+                    Grid.grid[x + 1, y + 1] = tile.transform;
+                }
+            }
+        }
+    }
+}
